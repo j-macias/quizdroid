8bfcc96122e260031e4520162a0e0cf84f34558f
jmacias <jmacias@uw.edu>
2017-02-15 09:34:06 -0800

    Initial commit

---
+++ app/src/main/java/edu/washington/jmacias/quizdroid/QuizApp.java
@@ -0,0 +1,20 @@
+package edu.washington.jmacias.quizdroid;
+
+import android.util.Log;
+
+public class QuizApp extends android.app.Application{
+    private TopicRepository instance = new TopicRepository();
+
+    public QuizApp() {
+        Log.d("QuizApp", "QuizApp constructor fired");
+    }
+
+    public TopicRepository getRepository() {
+        return instance;
+    }
+
+    @Override
+    public void onCreate() {
+        Log.d("QuizApp", "QuizApp onCreate fired");
+    }
+}
---
+++ app/src/main/java/edu/washington/jmacias/quizdroid/TopicRepository.java
@@ -0,0 +1,7 @@
+package edu.washington.jmacias.quizdroid;
+
+public class TopicRepository {
+
+
+
+}
